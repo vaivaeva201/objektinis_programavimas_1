fa49ea7beba812c8088b2823a6cc6c51575cf69d
Vaiva Evaltaite <vaivaevaltaite@Vaivas-MacBook-Pro.local>
2026-02-12 23:26:53 +0200

    Sukurta funkcija, kuri prašo vartotoją suvesti visus duomenis

--- main.cpp
+++ main.cpp
@@ -16,10 +16,38 @@ struct Studentas {
     double rez;
 };
 
+void duomenu_ivedimas(vector < Studentas > &grupe)
+{
+    Studentas A;
+    int mok_skaicius = 0;
+    cout << "Įveskite kiek studentų norėsite įvesti: ";
+    cin >> mok_skaicius; 
+
+    for (int i = 0; i < mok_skaicius; i++)
+    {
+        cout << "Įveskite studento vardą ir pavardę: ";
+        cin >> A.Vardas >> A.Pavarde;
+        cout << "Įveskite semestro pažymius:\nKiek pažymių bus? ";
+        int n, temp;
+        cin >> n;
+
+        for (int j= 0; j < n; j++)
+        {
+            cout << "Iveskite " << j+1 << " pazymi is " << n << ": ";
+            cin >> temp;
+            A.paz.push_back(temp);
+        }
+        cout << "Įveskite egzamoni pažymį: ";
+        cin >> A.egz_paz;
+        grupe.push_back(A);
+
+    }
+}
+
 int main(){
 
-    Studentas A;
     vector < Studentas > grupe;
+    duomenu_ivedimas(grupe);
     
     return 0;
 }
