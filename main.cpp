7c3e5a792a032b2769742993bfe1a1df1bbe5b18
Vaiva Evaltaite <vaivaevaltaite@Vaivas-MacBook-Pro.local>
2026-02-12 22:49:28 +0200

    Sukurta struktura

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+
+using std:: string;
+using std:: vector;
+using std:: cout;
+using std:: cin;
+
+struct Studentas {
+    string Vardas="A", Pavarde="BB";
+    // int *paz;
+    vector <int> paz;
+    int egz_paz;
+    double rez;
+};
+
+int main(){
+
+    Studentas A;
+    vector < Studentas > grupe;
+    
+    return 0;
+}
