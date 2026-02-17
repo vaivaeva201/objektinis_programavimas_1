f3dc2fc29e4f6fd23716aff00917db5d7c608a73
Vaiva Evaltaite <vaivaevaltaite@Vaivas-MacBook-Pro.local>
2026-02-17 16:57:48 +0200

    Sukurti ciklai kurie tikrina vartotoja ivestus duomenis

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <numeric>
 #include <algorithm>
+#include <limits>
 
 using std:: string;
 using std:: vector;
@@ -28,24 +29,80 @@ void duomenu_ivedimas(vector < Studentas > &grupe)
     Studentas A;
     int mok_skaicius = 0;
     cout << "Įveskite kiek studentų norėsite įvesti: ";
-    cin >> mok_skaicius; 
+    while (true)
+            {
+                cin >> mok_skaicius; 
+
+                if (cin.fail() || cin.peek() != '\n' || mok_skaicius < 1)
+                {
+                    cout << "Neteisingas skaičius! Įveskite naturalų skaičių: ";
+                    cin.clear();
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+                else
+                {
+                    break;
+                }
+            }
 
     for (int i = 0; i < mok_skaicius; i++)
     {
+        int n, temp;
         cout << "Įveskite studento vardą ir pavardę: ";
         cin >> A.Vardas >> A.Pavarde;
         cout << "Įveskite semestro pažymius:\nKiek pažymių bus? ";
-        int n, temp;
-        cin >> n;
+        while (true)
+            {
+                cin >> n; 
+
+                if (cin.fail() || cin.peek() != '\n' || n < 1)
+                {
+                    cout << "Neteisingas skaičius! Įveskite naturalų skaičių: ";
+                    cin.clear();
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+                else
+                {
+                    break;
+                }
+            }
 
         for (int j= 0; j < n; j++)
         {
             cout << "Iveskite " << j+1 << " pazymi is " << n << ": ";
-            cin >> temp;
+            while (true)
+            {
+                cin >> temp;
+
+                if (cin.fail() || cin.peek() != '\n' || temp < 1 || temp > 10)
+                {
+                    cout << "Neteisingas pažymys! Įveskite sveikąjį skaičių nuo 1 iki 10: ";
+                    cin.clear();
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+                else
+                {
+                    break;
+                }
+            }
             A.paz.push_back(temp);
         }
         cout << "Įveskite egzamoni pažymį: ";
-        cin >> A.egz_paz;
+        while (true)
+            {
+                cin >> A.egz_paz;   
+
+                if (cin.fail() || cin.peek() != '\n' || A.egz_paz < 1 || A.egz_paz > 10)
+                {
+                    cout << "Neteisingas pažymys! Įveskite sveikąjį skaičių nuo 1 iki 10: ";
+                    cin.clear();
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+                else
+                {
+                    break;
+                }
+            }
         grupe.push_back(A);
         A.paz.clear();
 
@@ -78,14 +135,28 @@ void rezultatai (vector < Studentas > &grupe)
 {
     int formatas;
     cout << "Prašau pasirinkite ar norite galutinį balą skaičiuoti su vidurkiu ar mediana. (Vidurkiu - 0, mediana - 1): ";
-    cin >> formatas;
+    while (true)
+            {
+                cin >> formatas;
 
-    cout << left << std::setw(20) << "Pavardė" << setw(20) << "Vardas";
+                if (cin.fail() || cin.peek() != '\n' || (formatas != 1 && formatas !=0))
+                {
+                    cout << "Neteisingas skaičius! Įveskite 0 arba 1: ";
+                    cin.clear();
+                    cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+    cout << left << setw(20) << "Pavardė" << setw(20) << "Vardas";
 
     if (formatas == 0)
     {
         cout << std::setw(20) << "Galutinis (Vid.)" << endl;
-        cout << "---------------------------------------------------------" << endl;
+        cout << "-----------------------------------------------------------" << endl;
         for (auto &x : grupe)
         {
             vidurkis(x);
@@ -96,7 +167,7 @@ void rezultatai (vector < Studentas > &grupe)
     else
     {
         cout << std::setw(20) << "Galutinis (Med.)" << endl;
-        cout << "---------------------------------------------------------" << endl;
+        cout << "-----------------------------------------------------------" << endl;
         for (auto &x : grupe)
         {
         mediana(x);
