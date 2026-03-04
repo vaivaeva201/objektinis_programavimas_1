8c01239cac0c2aac6f6ca09f5394b71e848a1de2
Vaiva Evaltaite <vaivaevaltaite@Vaivas-MacBook-Pro.local>
2026-03-04 22:00:11 +0200

    I funkcijos.h faila prideta struktura

---
+++ funkcijos.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+#include "antrastes.h"
+
+struct Studentas {
+    string Vardas="A", Pavarde="BB";
+    vector <int> paz;
+    int egz_paz;
+    double rez;
+    double Vidurkis, Mediana;
+};
+
+
+#endif
