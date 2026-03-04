b5c6eb3df30091e07aa2944f14651556d56bf8a5
Vaiva Evaltaite <vaivaevaltaite@Vaivas-MacBook-Pro.local>
2026-03-04 21:51:24 +0200

    Sukurtas vienas header failas

---
+++ antrastes.h
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <limits>
+#include <random>
+#include "RandInt.hpp"
+#include <fstream>
+#include <sstream>
+#include <chrono>
+
+using std:: string;
+using std:: vector;
+using std:: cout;
+using std:: cin;
+using std:: accumulate;
+using std:: setw;
+using std:: left;
+using std:: right;
+using std:: endl;
